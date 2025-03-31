b7bbb11a17a72103289b4926dce2c5ef4e62c5de
bbd666 <betterbequotin@orange.fr>
2025-03-31 11:52:07 +0200

    add multi_line.text
    + remove display_map from clean_display
    + rename refresh -> update_display

--- test_driver_LCD.py
+++ test_driver_LCD.py
@@ -4,14 +4,14 @@ from time import sleep
 from PIL import Image, ImageDraw,ImageFont
 
 lcd=LcdDisplay()
-lcd.clean_display()
 lcd.set_intensity(0.5)
 image_raw=Image.open("logo.bmp")
 image=image_raw.resize((132,80),Image.LANCZOS)
+#lcd.clean_display()
 lcd.load_image(image)
 lcd.draw_rectangle(60,20,120,10,1,1,2)
 lcd.draw_text(80,60,"HELLO WORLD",1,"/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",12,"ms",0,"left")
 lcd.draw_rectangle(30,60,130,70,1,1,2)
 lcd.draw_text(80,70,"HELLO WORLD",0,"/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",12,"ms",0,"left")
-lcd.refresh()
+lcd.update_diplay()
 #GPIO.cleanup()
